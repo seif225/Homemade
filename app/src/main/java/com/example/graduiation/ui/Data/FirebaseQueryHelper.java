6892adfd4269e51b8b74f3ae162a1ee8e692a8b2
Zeyad Aly <zeyadaly15@gmail.com>
2020-02-23 16:33:28 +0200

    Added SignIn Methods

--- app/src/main/java/com/example/graduiation/ui/Data/FirebaseQueryHelper.java
+++ app/src/main/java/com/example/graduiation/ui/Data/FirebaseQueryHelper.java
@@ -5,10 +5,12 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.example.graduiation.R;
+import com.example.graduiation.ui.intro.IntroActivity;
 import com.example.graduiation.ui.login.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -60,6 +62,36 @@ public class FirebaseQueryHelper {
                         }
                     });
         }
+
+    }
+    public void SignIn(String email, String password, Context context, ProgressDialog progressDialog) {
+        progressDialog.setTitle("Please wait");
+        progressDialog.setMessage("Signing you in");
+        progressDialog.show();
+        if (email.isEmpty()) {
+            Toast.makeText(context, "Enter a valid email", Toast.LENGTH_SHORT).show();
+        } else if (password.isEmpty()) {
+            Toast.makeText(context, "Enter a valid password", Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()) {
+                        SendUserToIntro(context);
+                    }else {
+                        Toast.makeText(context, "Error "+ task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void SendUserToIntro(Context context) {
+        Intent intro = new Intent(context, IntroActivity.class);
+        intro.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intro);
 
     }
 
