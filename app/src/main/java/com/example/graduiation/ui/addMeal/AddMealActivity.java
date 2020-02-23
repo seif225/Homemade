470538747dd10fb1d77e8755da4ff2824eadf7b5
seifeldenehab <seif.eld.ehab@gmail.com>
2020-02-23 14:38:31 +0200

    added missing files

---
+++ app/src/main/java/com/example/graduiation/ui/addMeal/AddMealActivity.java
@@ -0,0 +1,16 @@
+package com.example.graduiation.ui.addMeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.graduiation.R;
+
+public class AddMealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mean);
+    }
+}
---
+++ app/src/main/java/com/example/graduiation/ui/addMeal/AddMealViewHolder.java
@@ -0,0 +1,6 @@
+package com.example.graduiation.ui.addMeal;
+
+import androidx.lifecycle.ViewModel;
+
+public class AddMealViewHolder extends ViewModel {
+}
