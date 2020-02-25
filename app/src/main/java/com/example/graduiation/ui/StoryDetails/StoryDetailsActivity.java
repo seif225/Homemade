ea4f1ae86a8e2923017be5bb976c370cd51cd6d6
Zeyad Aly <zeyadaly15@gmail.com>
2020-02-25 23:32:58 +0200

    Added Stories

---
+++ app/src/main/java/com/example/graduiation/ui/StoryDetails/StoryDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.graduiation.ui.StoryDetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.graduiation.R;
+
+public class StoryDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story_details);
+    }
+}
